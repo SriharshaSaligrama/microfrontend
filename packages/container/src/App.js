6b816036d76718703eb24e69d199329515e5cfa6
Sriharsha <sriharshasaligrama@gmail.com>
2022-10-11 22:07:56 +0530

    test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -29,13 +29,13 @@ export default () => {
                     <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
                     <Suspense fallback={<Progress />}>
                         <Switch>
-                            <Route path='/auth'>
+                            {/* <Route path='/auth'>
                                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
                             </Route>
                             <Route path='/dashboard'>
                                 {!isSignedIn && <Redirect to='/' />}
                                 <DashboardLazy />
-                            </Route>
+                            </Route> */}
                             <Route path='/' component={MarketingLazy} />
                         </Switch>
                     </Suspense>
