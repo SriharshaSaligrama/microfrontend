2926f2a5502fe88b951718375ad532b3412eaa77
Sriharsha <sriharshasaligrama@gmail.com>
2022-10-11 22:10:45 +0530

    test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -6,8 +6,8 @@ import Progress from './components/Progress'
 import Header from './components/Header'
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'))
-const AuthLazy = lazy(() => import('./components/AuthApp'))
-const DashboardLazy = lazy(() => import('./components/DashboardApp'))
+// const AuthLazy = lazy(() => import('./components/AuthApp'))
+// const DashboardLazy = lazy(() => import('./components/DashboardApp'))
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
