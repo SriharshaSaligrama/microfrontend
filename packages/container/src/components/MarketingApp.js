b40a2a001e769ca52eae96b218bc3c65aa69e6ce
Sriharsha <sriharshasaligrama@gmail.com>
2022-10-12 00:26:53 +0530

    test

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -7,14 +7,14 @@ export default () => {
     const history = useHistory()
 
     useEffect(() => {
-        const { onParentNavigate: onNavigate } = marketingMount(ref.current, {
+        const onParentNavigate = marketingMount(ref.current, {
             initialPath: history.location.pathname,
             onNavigate: ({ pathname: nextPathname }) => {
                 const { pathname } = history.location
                 if (pathname !== nextPathname) history.push(nextPathname)
             }
         })
-        history.listen(onNavigate)
+        history.listen(onParentNavigate)
     }, [])
     return <div ref={ref} />
 }
